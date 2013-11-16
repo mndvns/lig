e56cc64fb72ec40cc51fdea22126d0e415a1bfbb
Michael Vanasse <michaelvanasse@gmail.com>
2013-11-16 02:33:59 -0700

    add multiple lines

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var colors = require('colors');
 var util = require('util');
+var xtend = require('xtend');
 
 /**
  * Expose `Log`.
@@ -13,31 +14,38 @@ exports = module.exports = Log;
 
 /**
  * Log
+ * @param {Object} options optional
  * @param {String} name
  * @param {String} color optional; defaults to blue
- * @param {String} message
  */
 
-function Log(name, message) {
-  var color = colors[message]
-    ? message
-    : 'blue';
-
-  var args = 1;
-  if (message === "blue") args += 1;
-  if (color   !== "blue") args += 1;
-
-  var width = 9;
-  var len = Math.max(0, width - name.length);
-  var pad = Array(len + 1).join(' ');
-  var messages = Array.prototype.slice.call(arguments, args);
-
-  messages.map(function(msg){
-    return util.inspect(msg);
-  });
-  if ((messages != null ? messages.length : void 8) === 1) {
-    messages = messages[0];
+function Log(options, name, message) {
+  var opts = {
+    style: 'blue',
+    width: 9,
+    padding: 1,
+    args: 2
   }
-  console.log(pad, name[color], messages);
-  return [name[color], messages].join(' ');
+
+  if (typeof options !== "object") {
+    if (!message) {
+      opts.args -= 1;
+      message = name;
+      name = options;
+    } else {
+      opts.style = name;
+    }
+    options = {};
+  } else {
+    opts = xtend(opts, options)
+  }
+
+  var messages = Array.prototype.slice.call(arguments, opts.args)
+
+  var len = Math.max(0, opts.width - name.length);
+  var pad = Array(len + opts.padding).join(' ');
+  var tab = Array(len + opts.padding + name.length + 2).join(' ');
+
+  console.log(pad, name[opts.style], messages.join('\n' + tab));
+  return [name[opts.style], messages].join(' ');
 };
--- test.js
+++ test.js
@@ -2,26 +2,28 @@
 var lig = require('./index');
 
 describe('lig', function() {
-  it('should display console messages', function() {
+  it('should display with two arguments', function() {
 
     console.log();
 
-    lig('default', 'this is the default blue')
-      .should.include('this is the default blue');
-
     lig('strong', 'red', 'while this is red')
-      .should.include('while this is red');
 
-    lig('yup', [1, 2, 3]).should.include('yup');
+    lig('yup', [1, 2, 3])
 
     lig('green', 'green', 'everything lines up nicely on the left label')
-      .should.include('green');
 
     lig('bold', 'bold', 'more than just colors')
-      .should.include('bold');
 
     lig('hiyooooo', 'inverse', 'anything in the `colors` library is fair game')
-      .should.include('colors')
 
   });
+
+  it('should display with a options object', function() {
+
+    console.log();
+
+    lig({style: 'red'}, 'options', 'can be passed in')
+
+    lig({style: 'yellow', messageLength: 2}, 'yellow', 'and work just the same', 'have multiple lines', 'but keep the same clean indentation')
+  })
 });
