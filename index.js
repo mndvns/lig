303003e670bd8e5b1b5da49b369ea79291f6bc71
Michael Vanasse <michaelvanasse@gmail.com>
2013-11-19 00:45:57 -0700

    update tests

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 var colors = require('colors');
-var util = require('util');
-var xtend = require('xtend');
+var inspect = require('util').inspect;
+var extend = require('xtend');
 
 /**
  * Expose `Log`.
@@ -12,40 +12,60 @@ var xtend = require('xtend');
 
 exports = module.exports = Log;
 
+var defaults = {
+  style: 'blue',
+  width: 9,
+  padding: 1
+};
+
 /**
  * Log
  * @param {Object} options optional
- * @param {String} name
- * @param {String} color optional; defaults to blue
+ * @param {String} message ...
  */
 
-function Log(options, name, message) {
-  var opts = {
-    style: 'blue',
-    width: 9,
-    padding: 1,
-    args: 2
-  }
+function Log() {
 
-  if (typeof options !== "object") {
-    if (!message) {
-      opts.args -= 1;
-      message = name;
-      name = options;
-    } else {
-      opts.style = name;
-    }
-    options = {};
-  } else {
-    opts = xtend(opts, options)
-  }
+  var args = Array.prototype.slice.call(arguments);
+  if (!(this instanceof Log)) return print(args);
 
-  var messages = Array.prototype.slice.call(arguments, opts.args)
+  this.options = type(args[0]) === 'object' ? args[0] : {};
+  this.log = log.bind(this)
+  this.print = print.bind(this)
+  return this.log;
+}
 
-  var len = Math.max(0, opts.width - name.length);
+function log() {
+  var args = Array.prototype.slice.call(arguments)
+  return this.print(args);
+}
+
+function print(args) {
+
+  var label = '';
+  var buf = [];
+  var opts = extend(defaults, this.options || {});
+
+  var self = this;
+  args.forEach(function(arg, i, ctx) {
+    if (type(arg) === "object") return opts = extend(opts, arg);
+    if (ctx.length === 1) return buf.push(arg);
+    if (!label.length) return label = arg;
+    if (type(arg) === "string") return buf.push(arg);
+    if (type(arg) === "array") return buf = buf.concat(arg);
+  })
+
+  var len = Math.max(0, opts.width - label.length);
   var pad = Array(len + opts.padding).join(' ');
-  var tab = Array(len + opts.padding + name.length + 2).join(' ');
+  var tab = Array(len + opts.padding +  label.length + 2).join(' ');
 
-  console.log(pad, name[opts.style], messages.join('\n' + tab));
-  return [name[opts.style], messages].join(' ');
+  console.log(pad, label[opts.style], buf.join('\n' + tab));
+
+  // return an array so we can test against it
+  return [label].concat(buf);
 };
+
+function type(obj) {
+  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+};
+
--- test.js
+++ test.js
@@ -6,15 +6,15 @@ describe('lig', function() {
 
     console.log();
 
-    lig('strong', 'red', 'while this is red')
+    // lig('strong', 'red', 'while this is red')
 
-    lig('yup', [1, 2, 3])
+    // lig('yup', [1, 2, 3])
 
-    lig('green', 'green', 'everything lines up nicely on the left label')
+    // lig('green', 'green', 'everything lines up nicely on the left label')
 
-    lig('bold', 'bold', 'more than just colors')
+    // lig('bold', 'bold', 'more than just colors')
 
-    lig('hiyooooo', 'inverse', 'anything in the `colors` library is fair game')
+    // lig('hiyooooo', 'inverse', 'anything in the `colors` library is fair game')
 
   });
 
@@ -22,8 +22,23 @@ describe('lig', function() {
 
     console.log();
 
-    lig({style: 'red'}, 'options', 'can be passed in')
+    // lig({style: 'red'}, 'options', 'can be passed in')
 
-    lig({style: 'yellow', messageLength: 2}, 'yellow', 'and work just the same', 'have multiple lines', 'but keep the same clean indentation')
+    // lig({style: 'yellow', messageLength: 2}, 'yellow', 'and work just the same', 'have multiple lines', 'but keep the same clean indentation')
+
+    lig('single message');
+    lig('standard', 'label and a message');
+    lig('multiple', 'my message goes here', 'and here as well', 'i can pass as many as i want');
+    lig('array', ['or i can', 'pass an array', 'like this']);
+    lig({style: 'red'}, 'options', 'i can even pass in extra options');
+
+    console.log('\n');
+
+    var error = new lig({style: 'red'})
+    error('error', 'work just fine too')
+    error('error', ['work just fine too', 'just like this'])
+
+
+
   })
 });
